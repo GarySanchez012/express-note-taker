9a1d4ee6d0dc12693b67b0bbee1dbfc9c50b9067
arroyhondo <garysanchez012@gmail.com>
2021-09-09 18:25:41 -0400

    added crud routes for get, post, and delete requests

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,40 @@
 const router = require("express").Router();
+const fs = require("fs");
 const path = require("path");
+const db = require("../../db/db.json");
+let id = db.length + 1;
+
+const output = "./db/db.json";
+
+router.get("/notes", (req, res) => {
+  res.json(db);
+});
+
+router.post("/notes", (req, res) => {
+  req.body.id = id++;
+
+  db.push(req.body);
+
+  fs.writeFile(output, JSON.stringify(db), (err) => {
+    if (err) throw err;
+  });
+  res.json(db);
+});
+
+router.delete("/notes/:id", (req, res) => {
+    let id = req.params.id
+    
+    for (let i = 0; i < db.length; i++) {
+        if(db[i].id === parseInt(id)){
+            db.splice(i, 1)
+            
+        }
+    }
+
+    fs.writeFile(output, JSON.stringify(db), (err) => {
+        if (err) throw err;
+      });
+      res.json(db);
+})
 
 module.exports = router;
